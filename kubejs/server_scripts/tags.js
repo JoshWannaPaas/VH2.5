73afe679c127c08cc6e42ebc372bed47970f3f23
a1qs <ankiller2007.09.20@gmail.com>
2024-04-30 15:22:52 +0200

    added kubejs, ported 00_remove_recipes

---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,34 @@
+// priority: 1
+/* scripts made by Douwsky
+for Iskall85's Vaulthunters 
+ported by a1qs to KubeJS */
+
+console.info('Adding Tags!')
+
+const colors = ["white","orange","magenta","light_blue","yellow","lime","pink","gray","cyan","purple","blue","brown","green","black","light_gray"]
+
+onEvent('item.tags', event => {
+
+    event.add('ag:crafter', [
+        '#refinedstorage:crafter',
+        'rftoolsutility:crafter1',
+        'rftoolsutility:crafter2',
+        'rftoolsutility:crafter3',
+        'create:mechanical_crafter',
+        'appliedenergistics2:molecular_assembler',
+        'thermal:machine_crafter',
+        'botania:crafting_halo',
+        'botania:auto_crafting_halo',
+        'mekanism:formulaic_assemblicator'
+    ])
+    
+    colors.forEach(colors =>{
+        event.add('ag:concrete',
+            `minecraft:${colors}_concrete`
+    )})
+
+    colors.forEach(colors =>{
+        event.add('ag:concrete_powder',
+            `minecraft:${colors}_concrete_powder`
+    )})
+})
