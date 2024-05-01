75bd7fe60122476c4770b4544cb98afcef382460
a1qs <ankiller2007.09.20@gmail.com>
2024-05-01 13:36:17 +0200

    temp version of challenge crystal coop conversion

--- kubejs/server_scripts/vh2.5.js
+++ kubejs/server_scripts/vh2.5.js
@@ -1,6 +1,10 @@
 /* scripts made by a1qs
 for Vault Hunters 2.5 */
 
+//temp
+let chalCrys0 = Item.of('the_vault:vault_crystal', '{CrystalData:{rooms:[],canBeModified:0b,canTriggerInfluences:0b,canGenerateTreasureRooms:0b,Challenge:1b,PlayerBossName:"",Objective:"the_vault:summon_and_kill_boss",Type:"CLASSIC",preventsRandomModifiers:1b,targetObjectiveCount:6,echoData:{echoCount:0},Modifiers:[{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Safe Zone"},{Operation:0,Name:"Locked"},{Operation:0,Name:"Rush"},{Operation:0,Name:"Rush"}]}}')
+let chalCrys0_COOP = Item.of('the_vault:vault_crystal', '{CrystalData:{rooms:[],Frame:{Tiles:[]},canBeModified:0b,canTriggerInfluences:0b,canGenerateTreasureRooms:0b,Challenge:1b,PlayerBossName:"",Objective:"the_vault:summon_and_kill_boss",Type:"COOP",preventsRandomModifiers:1b,targetObjectiveCount:6,echoData:{echoCount:0},Modifiers:[{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Safe Zone"},{Operation:0,Name:"Locked"},{Operation:0,Name:"Rush"},{Operation:0,Name:"Rush"}]}}')
+
 onEvent('recipes', event => {
 
     event.shaped(Item.of('the_vault:vault_paxel', '{Damage:0,ShouldEnhance:0b,Enhancement:{Id:"the_vault:fortunate"},display:{Name:\'["",{"text":"Fortunate Paxel","italic":false,"color":"#ffb31a","bold":true}]\'}}') , [
@@ -14,4 +18,6 @@ onEvent('recipes', event => {
         V: 'the_vault:vault_diamond_block'
     })
 
+    // temp
+    event.shaped(chalCrys0_COOP, [chalCrys0])
 })
