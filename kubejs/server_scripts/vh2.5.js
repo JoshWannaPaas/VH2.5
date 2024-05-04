41912aab22d85a62a09738670a09eb40af9180b4
a1qs <ankiller2007.09.20@gmail.com>
2024-05-04 19:07:09 +0200

    added TH/Artisan flask recipes

--- kubejs/server_scripts/vh2.5.js
+++ kubejs/server_scripts/vh2.5.js
@@ -1,10 +1,6 @@
 /* scripts made by a1qs
 for Vault Hunters 2.5 */
 
-//temp
-let chalCrys0 = Item.of('the_vault:vault_crystal', '{CrystalData:{rooms:[],canBeModified:0b,canTriggerInfluences:0b,canGenerateTreasureRooms:0b,Challenge:1b,PlayerBossName:"",Objective:"the_vault:summon_and_kill_boss",Type:"CLASSIC",preventsRandomModifiers:1b,targetObjectiveCount:6,echoData:{echoCount:0},Modifiers:[{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Safe Zone"},{Operation:0,Name:"Locked"},{Operation:0,Name:"Rush"},{Operation:0,Name:"Rush"}]}}')
-let chalCrys0_COOP = Item.of('the_vault:vault_crystal', '{CrystalData:{rooms:[],Frame:{Tiles:[]},canBeModified:0b,canTriggerInfluences:0b,canGenerateTreasureRooms:0b,Challenge:1b,PlayerBossName:"",Objective:"the_vault:summon_and_kill_boss",Type:"COOP",preventsRandomModifiers:1b,targetObjectiveCount:6,echoData:{echoCount:0},Modifiers:[{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Treasure"},{Operation:0,Name:"Safe Zone"},{Operation:0,Name:"Locked"},{Operation:0,Name:"Rush"},{Operation:0,Name:"Rush"}]}}')
-
 onEvent('recipes', event => {
 
     event.shaped(Item.of('the_vault:vault_paxel', '{Damage:0,ShouldEnhance:0b,Enhancement:{Id:"the_vault:fortunate"},display:{Name:\'["",{"text":"Fortunate Paxel","italic":false,"color":"#ffb31a","bold":true}]\'}}') , [
@@ -18,9 +14,27 @@ onEvent('recipes', event => {
         V: 'the_vault:vault_diamond_block'
     })
 
-   
+    //temp recipes, up to debate
+    event.shaped(Item.of('the_vault:reset_flask', '{Skillable:"Artisan"}') , [
+        'OPE',
+        'PFP',
+        'EPO'
+    ], {
+        O: 'the_vault:omega_pog',
+        P: 'the_vault:vault_platinum',
+        E: 'the_vault:extraordinary_echo_gem',
+        F: 'the_vault:reset_flask'
+    })
 
-    // temp
-    // event.shaped(Item.of('the_vault:reset_flask', '{Skillable:"Artisan"}'), ['the_vault:vault_diamond'])
-    event.shaped(chalCrys0_COOP, [chalCrys0])
+    event.shaped(Item.of('the_vault:reset_flask', '{Skillable:"Treasure Hunter"}') , [
+        'EPO',
+        'PFP',
+        'OPE'
+    ], {
+        O: 'the_vault:omega_pog',
+        P: 'the_vault:vault_platinum',
+        E: 'the_vault:extraordinary_echo_gem',
+        F: 'the_vault:reset_flask'
+    })
+    
 })
