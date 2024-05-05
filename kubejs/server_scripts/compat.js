82274625d4433240ab5aeb5977bb1b230d112f00
a1qs <ankiller2007.09.20@gmail.com>
2024-05-05 11:37:18 +0200

    make mod resources not completly miserable

---
+++ kubejs/server_scripts/compat.js
@@ -0,0 +1,156 @@
+/* scripts made by a1qs
+for Vault Hunters 2.5 */
+
+onEvent('recipes', event => {
+
+    // copper
+    event.shapeless('9x mekanism:nugget_copper', ['#forge:ingots/copper'])
+    event.shapeless('9x immersiveengineering:nugget_copper', ['#forge:ingots/copper'])
+    event.shapeless('9x thermal:copper_nugget', ['#forge:ingots/copper'])
+    event.shapeless('9x create:copper_nugget', ['#forge:ingots/copper'])
+
+    event.shapeless('1x mekanism:ingot_copper', ['9x #forge:nuggets/copper'])
+    event.shapeless('1x immersiveengineering:ingot_copper', ['9x #forge:nuggets/copper'])
+    event.shapeless('1x create:copper_ingot', ['9x #forge:nuggets/copper'])
+    event.shapeless('1x thermal:copper_ingot', ['9x #forge:nuggets/copper'])
+
+    event.shapeless('1x mekanism:block_copper', ['9x #forge:ingots/copper'])
+    event.shapeless('1x immersiveengineering:storage_copper', ['9x #forge:ingots/copper'])
+    event.shapeless('1x create:copper_block', ['9x #forge:ingots/copper'])
+    event.shapeless('1x thermal:copper_block', ['9x #forge:ingots/copper'])
+
+    event.shapeless('9x mekanism:ingot_copper', ['#forge:storage_blocks/copper'])
+    event.shapeless('9x immersiveengineering:ingot_copper', ['#forge:storage_blocks/copper'])
+    event.shapeless('9x thermal:copper_ingot', ['#forge:storage_blocks/copper'])
+    event.shapeless('9x create:copper_ingot', ['#forge:storage_blocks/copper'])
+
+    // uranium
+    event.shapeless('9x immersiveengineering:nugget_uranium', ['#forge:ingots/uranium'])
+    event.shapeless('9x mekanism:nugget_uranium', ['#forge:ingots/uranium'])
+
+    event.shapeless('1x immersiveengineering:ingot_uranium', ['9x #forge:nuggets/uranium'])
+    event.shapeless('1x mekanism:ingot_uranium', ['9x #forge:nuggets/uranium'])
+
+    event.shapeless('1x immersiveengineering:storage_uranium', ['9x #forge:ingots/uranium'])
+    event.shapeless('1x mekanism:block_uranium', ['9x #forge:ingots/uranium'])
+
+    event.shapeless('9x mekanism:ingot_uranium', ['#forge:storage_blocks/uranium'])
+    event.shapeless('9x immersiveengineering:ingot_uranium', ['#forge:storage_blocks/uranium'])
+
+    // silver
+    event.shapeless('9x immersiveengineering:nugget_silver', ['#forge:ingots/silver'])
+    event.shapeless('9x thermal:silver_nugget', ['#forge:ingots/silver'])
+
+    event.shapeless('1x immersiveengineering:ingot_silver', ['9x #forge:nuggets/silver'])
+    event.shapeless('1x thermal:silver_ingot', ['9x #forge:nuggets/silver'])
+
+    event.shapeless('1x immersiveengineering:storage_silver', ['9x #forge:ingots/silver'])
+    event.shapeless('1x thermal:silver_block', ['9x #forge:ingots/silver'])
+
+    event.shapeless('9x immersiveengineering:ingot_silver', ['#forge:storage_blocks/silver'])
+    event.shapeless('9x thermal:silver_ingot', ['#forge:storage_blocks/silver'])
+
+    // tin
+    event.shapeless('9x mekanism:nugget_tin', ['#forge:ingots/tin'])
+    event.shapeless('9x thermal:tin_nugget', ['#forge:ingots/tin'])
+
+    event.shapeless('1x mekanism:ingot_tin', ['9x #forge:nuggets/tin'])
+    event.shapeless('1x thermal:tin_ingot', ['9x #forge:nuggets/tin'])
+
+    event.shapeless('1x mekanism:block_tin', ['9x #forge:ingots/tin'])
+    event.shapeless('1x thermal:tin_block', ['9x #forge:ingots/tin'])
+
+    event.shapeless('9x mekanism:ingot_tin', ['#forge:storage_blocks/tin'])
+    event.shapeless('9x thermal:tin_ingot', ['#forge:storage_blocks/tin'])
+
+    // nickel
+    event.shapeless('9x immersiveengineering:nugget_nickel', ['#forge:ingots/nickel'])
+    event.shapeless('9x thermal:nickel_nugget', ['#forge:ingots/nickel'])
+
+    event.shapeless('1x immersiveengineering:ingot_nickel', ['9x #forge:nuggets/nickel'])
+    event.shapeless('1x thermal:nickel_ingot', ['9x #forge:nuggets/nickel'])
+
+    event.shapeless('1x immersiveengineering:storage_nickel', ['9x #forge:ingots/nickel'])
+    event.shapeless('1x thermal:nickel_block', ['9x #forge:ingots/nickel'])
+
+    event.shapeless('9x immersiveengineering:ingot_nickel', ['#forge:storage_blocks/nickel'])
+    event.shapeless('9x thermal:nickel_ingot', ['#forge:storage_blocks/nickel'])
+
+    // constantan
+    event.shapeless('9x immersiveengineering:nugget_constantan', ['#forge:ingots/constantan'])
+    event.shapeless('9x thermal:constantan_nugget', ['#forge:ingots/constantan'])
+
+    event.shapeless('1x immersiveengineering:ingot_constantan', ['9x #forge:nuggets/constantan'])
+    event.shapeless('1x thermal:constantan_ingot', ['9x #forge:nuggets/constantan'])
+
+    event.shapeless('1x immersiveengineering:storage_constantan', ['9x #forge:ingots/constantan'])
+    event.shapeless('1x thermal:constantan_block', ['9x #forge:ingots/constantan'])
+
+    event.shapeless('9x immersiveengineering:ingot_constantan', ['#forge:storage_blocks/constantan'])
+    event.shapeless('9x thermal:constantan_ingot', ['#forge:storage_blocks/constantan'])
+
+    // electrum
+    event.shapeless('9x immersiveengineering:nugget_electrum', ['#forge:ingots/electrum'])
+    event.shapeless('9x thermal:electrum_nugget', ['#forge:ingots/electrum'])
+
+    event.shapeless('1x immersiveengineering:ingot_electrum', ['9x #forge:nuggets/electrum'])
+    event.shapeless('1x thermal:electrum_ingot', ['9x #forge:nuggets/electrum'])
+
+    event.shapeless('1x immersiveengineering:storage_electrum', ['9x #forge:ingots/electrum'])
+    event.shapeless('1x thermal:electrum_block', ['9x #forge:ingots/electrum'])
+
+    event.shapeless('9x immersiveengineering:ingot_electrum', ['#forge:storage_blocks/electrum'])
+    event.shapeless('9x thermal:electrum_ingot', ['#forge:storage_blocks/electrum'])
+
+    // steel
+    event.shapeless('9x immersiveengineering:nugget_steel', ['#forge:ingots/steel'])
+    event.shapeless('9x mekanism:nugget_steel', ['#forge:ingots/steel'])
+
+    event.shapeless('1x immersiveengineering:ingot_steel', ['9x #forge:nuggets/steel'])
+    event.shapeless('1x mekanism:ingot_steel', ['9x #forge:nuggets/steel'])
+
+    event.shapeless('1x immersiveengineering:storage_steel', ['9x #forge:ingots/steel'])
+    event.shapeless('1x mekanism:block_steel', ['9x #forge:ingots/steel'])
+
+    event.shapeless('9x mekanism:ingot_steel', ['#forge:storage_blocks/steel'])
+    event.shapeless('9x immersiveengineering:ingot_steel', ['#forge:storage_blocks/steel'])
+
+    // bronze
+    event.shapeless('9x mekanism:nugget_bronze', ['#forge:ingots/bronze'])
+    event.shapeless('9x thermal:bronze_nugget', ['#forge:ingots/bronze'])
+
+    event.shapeless('1x mekanism:ingot_bronze', ['9x #forge:nuggets/bronze'])
+    event.shapeless('1x thermal:bronze_ingot', ['9x #forge:nuggets/bronze'])
+
+    event.shapeless('1x mekanism:block_bronze', ['9x #forge:ingots/bronze'])
+    event.shapeless('1x thermal:bronze_block', ['9x #forge:ingots/bronze'])
+
+    event.shapeless('9x mekanism:ingot_bronze', ['#forge:storage_blocks/bronze'])
+    event.shapeless('9x thermal:bronze_ingot', ['#forge:storage_blocks/bronze'])
+
+    // lead
+    event.shapeless('9x immersiveengineering:nugget_lead', ['#forge:ingots/lead'])
+    event.shapeless('9x mekanism:nugget_lead', ['#forge:ingots/lead'])
+    event.shapeless('9x thermal:lead_nugget', ['#forge:ingots/lead'])
+
+    event.shapeless('1x immersiveengineering:ingot_lead', ['9x #forge:nuggets/lead'])
+    event.shapeless('1x mekanism:ingot_lead', ['9x #forge:nuggets/lead'])
+    event.shapeless('1x thermal:lead_ingot', ['9x #forge:nuggets/lead'])
+
+    event.shapeless('1x immersiveengineering:storage_lead', ['9x #forge:ingots/lead'])
+    event.shapeless('1x mekanism:block_lead', ['9x #forge:ingots/lead'])
+    event.shapeless('1x thermal:lead_block', ['9x #forge:ingots/lead'])
+
+    event.shapeless('9x immersiveengineering:ingot_lead', ['#forge:storage_blocks/lead'])
+    event.shapeless('9x mekanism:ingot_lead', ['#forge:storage_blocks/lead'])
+    event.shapeless('9x thermal:lead_ingot', ['#forge:storage_blocks/lead'])
+
+
+
+
+
+    
+
+
+
+})
