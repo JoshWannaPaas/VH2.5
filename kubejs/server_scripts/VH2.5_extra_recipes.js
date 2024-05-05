7f569b621ef40bd721718f9d2257b73d0b307a82
DefinitelyNotKira <remmxx@gmail.com>
2024-05-05 21:02:52 +0400

    Add files via upload

---
+++ kubejs/server_scripts/VH2.5_extra_recipes.js
@@ -0,0 +1,12 @@
+onEvent('recipes', event => {
+    event.shaped('minecraft:elytra', [
+        'LSL',
+        'WAW',
+        'W W'
+    ], {
+        L: 'the_vault:perfect_larimar',
+        S: 'minecraft:string',
+        W: 'minecraft:wool',
+        A: 'the_vault:perfect_alexandrite'
+    })
+})
