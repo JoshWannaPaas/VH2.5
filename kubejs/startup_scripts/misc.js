db089c7e3d647a8aac7f2b81d892b2b4299335b1
a1qs <ankiller2007.09.20@gmail.com>
2024-05-25 19:18:30 +0200

    change stack sizes fo pearls and snowballs to 64

--- kubejs/startup_scripts/misc.js
+++ kubejs/startup_scripts/misc.js
@@ -1,5 +1,14 @@
 onEvent('block.modification', event => {
-    event.modify('the_vault:vault_chest', block => {
-        block.destroySpeed = 35
-      })
+  event.modify('the_vault:vault_chest', block => {
+    block.destroySpeed = 35
+  })
+})
+
+onEvent('item.modification', event => {
+  event.modify('minecraft:ender_pearl', item => {
+    item.maxStackSize = 64
+  })
+  event.modify('minecraft:snowball', item => {
+    item.maxStackSize = 64
+  })
 })
