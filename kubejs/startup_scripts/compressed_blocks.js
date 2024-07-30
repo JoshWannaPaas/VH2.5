c62b5a5efb1a7202198bb02e775e240da207e186
JoshWannaPaas <paasjoshua@yahoo.com>
2024-07-30 15:08:18 -0700

    added compressed blocks
    blackstone, basalt, deepslate, and cobbled deepslate

---
+++ kubejs/startup_scripts/compressed_blocks.js
@@ -0,0 +1,230 @@
+console.info('Adding compressed blocks!')
+
+
+onEvent('block.registry', event => {
+
+    // Deepslate
+    event.create('compressium:deepslate_1')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (1x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:deepslate_2')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (2x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:deepslate_3')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (3x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:deepslate_4')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (4x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:deepslate_5')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (5x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:deepslate_6')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (6x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:deepslate_7')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (7x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:deepslate_8')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (8x)')
+       .harvestTool('pickaxe', 0) 
+   
+          
+   event.create('compressium:deepslate_9')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Deepslate (9x)')
+       .harvestTool('pickaxe', 0) 
+   
+    
+   // Cobbled Deepslate
+    event.create('compressium:cobbled_deepslate_1')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (1x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:cobbled_deepslate_2')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (2x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:cobbled_deepslate_3')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (3x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:cobbled_deepslate_4')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (4x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:cobbled_deepslate_5')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (5x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:cobbled_deepslate_6')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (6x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:cobbled_deepslate_7')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (7x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:cobbled_deepslate_8')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (8x)')
+       .harvestTool('pickaxe', 0) 
+   
+          
+   event.create('compressium:cobbled_deepslate_9')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Cobbled Deepslate (9x)')
+       .harvestTool('pickaxe', 0) 
+
+   // Blackstone
+    event.create('compressium:blackstone_1')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (1x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:blackstone_2')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (2x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:blackstone_3')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (3x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:blackstone_4')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (4x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:blackstone_5')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (5x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:blackstone_6')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (6x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:blackstone_7')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (7x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:blackstone_8')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (8x)')
+       .harvestTool('pickaxe', 0) 
+   
+          
+   event.create('compressium:blackstone_9')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Blackstone (9x)')
+       .harvestTool('pickaxe', 0) 
+
+   // Basalt
+       event.create('compressium:basalt_1')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (1x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:basalt_2')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (2x)')
+       .harvestTool('pickaxe', 0) 
+
+   event.create('compressium:basalt_3')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (3x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:basalt_4')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (4x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:basalt_5')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (5x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:basalt_6')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (6x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:basalt_7')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (7x)')
+       .harvestTool('pickaxe', 0) 
+          
+   event.create('compressium:basalt_8')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (8x)')
+       .harvestTool('pickaxe', 0) 
+   
+          
+   event.create('compressium:basalt_9')
+  	   .material('stone')
+       .hardness(2.0)
+       .displayName('Compressed Basalt (9x)')
+       .harvestTool('pickaxe', 0) 
+})
